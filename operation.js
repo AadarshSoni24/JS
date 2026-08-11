89e80f4f1a30d41fccf8ac0bdace06eaf65cc27e
Adarsh <soniaadarsh652@gmail.com>
2026-08-11 19:12:13 +0530

    Add operation.js with code experiments

---
+++ operation.js
@@ -0,0 +1,35 @@
+let value =5 
+console.log(value);
+
+
+console.log(2+2);
+console.log(2*2);
+console.log(2/2);
+console.log(2-2);
+console.log(2**2);
+console.log(2%4);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+//In JavaScript, this behavior is determined by left-to-right evaluation order and implicit type coercion
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
